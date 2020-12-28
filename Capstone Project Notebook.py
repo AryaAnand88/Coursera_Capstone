b4683d04c4848e9829da634e74dc01dad0984fa5
DonCorleone880 <72746189+DonCorleone880@users.noreply.github.com>
2020-12-28 10:56:39 +0800

    Add files via upload

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## <b>PPEER GRADED CAPSTONE PROJECT NOTEBOOK </b>
+# 
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
